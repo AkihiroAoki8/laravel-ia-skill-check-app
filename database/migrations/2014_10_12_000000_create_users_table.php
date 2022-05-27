9e9b9afb46c6ac29264d88905ec90b0136d1000e
AkihiroAoki8 <103711504+AkihiroAoki8@users.noreply.github.com>
2022-05-27 10:41:51 +0900

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('role');
-            $table->unsignedBigInteger(('department_id'));
+            $table->unsignedBigInteger('department_id');
             $table->rememberToken();
             $table->timestamps();
         });
