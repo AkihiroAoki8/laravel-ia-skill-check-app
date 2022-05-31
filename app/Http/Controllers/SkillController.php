9b0efa5236e66af7d46d7c4236acc095007fb283
k.yoshida <>
2022-05-31 13:20:31 +0900

    view fix

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -8,35 +8,43 @@ use App\Models\Skill;
 
 class SkillController extends Controller
 {
-    public function index(){
-        $skills=Skill::all();
+    public function index()
+    {
+        $skills = Skill::all();
 
-        return view('skills.index',compact('skills'));
+        return view('skills.index', compact('skills'));
     }
 
-    
-    public function create(){
+
+    public function create()
+    {
         return view('skills.create');
     }
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         Skill::create([
-            'name'=>$request['name']
+            'name' => $request['name']
         ]);
         return redirect()->route('skills.index');
     }
-    public function delete($id){
-        $skill=Skill::findOrFail($id);
+    public function delete($id)
+    {
+        $skill = Skill::findOrFail($id);
         $skill->delete();
-        
+
+
+
         return redirect()->route('skills.index');
     }
-    public function edit($id){
-        $oldskill=Skill::findOrFail($id);
-        return view('skills.edit',compact('oldskill'));
+    public function edit($id)
+    {
+        $oldskill = Skill::findOrFail($id);
+        return view('skills.edit', compact('oldskill'));
     }
-    public function update($id,Request $request){
-        $skill=Skill::findOrFail($id);
-        $skill->name=$request['name'];
+    public function update($id, Request $request)
+    {
+        $skill = Skill::findOrFail($id);
+        $skill->name = $request['name'];
         $skill->save();
 
         session()->flash('flash_message', '更新完了');
