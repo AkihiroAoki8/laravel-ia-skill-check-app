a757d699639982363798a0f2417bb05e5470cfdd
h.fukuda <h.fukuda@L-0004.local>
2022-05-31 13:50:35 +0900

    [clean]navigationの修正

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,7 +12,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('users.skill', ['id' => Auth::id()])" :active="request()->routeIs('dashboard')">
                         {{ __('Users') }}
                     </x-nav-link>
                 </div>
